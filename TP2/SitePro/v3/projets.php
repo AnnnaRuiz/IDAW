d64644b8a19bf2b3a8616939d641905db6c882be
Anna Ruiz <anna.ruiz@free.fr>
2023-10-10 10:46:02 +0200

    v3 avec les factorisations

---
+++ TP2/SitePro/v3/projets.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+
+<?php require_once('head.php')?>
+    
+
+
+<body>
+    <div class="container p-0">
+        <div class="row bg-dark text-white m-0 p-0" >
+            <div class=" col-12 text-center"></br><h2> <i class="fas fa-briefcase" style="color: #ffffff;"></i> Mes projets et expériences </h2></br></div>
+        </div>
+    </div> 
+ 
+    <div class="container p-0 ">
+        <div class="row align-self-center">
+            <div class="col-3 ml-0">
+                <?php require_once('template_menu.php')?>
+            </div>
+
+            <div class="col-9 text-justify">
+                </br>
+                <div class="row-4">
+                    <h4>2023 : Projet en SmallTalk - Jeu de Pong</h4>
+                    <p>Mené dans le cadre d'un cours de Génie Logiciel</p>
+                    <a>Lien vers la vidéo du projet : https://we.tl/t-yiBvlaA6Vo</a>
+                </div>
+                </br>
+                <div class="row-4">
+                    <h4> Mai-Aout 2023 : Stage découverte de Domaine (L3) - EDF, Nanterre <img style="height: 40px" src="EDF.png"></h4>
+                    <p>Refonte du portail web dédié à la salle des marchés en suivant une démarche d'UXDesign</p>
+                    <p>Projet full-stack : HTML, CSS (Boostrap), Java, Apache Tomcat</p>
+                </div>
+                </br>
+                <div class="row-4">
+                    <h4>2021 - 2022 : Projet (Travail d’Initiative Personnelle Encadré)</h4>
+                    <p>Analyse informatique d’IRM cérébrale</p>
+                    <p>Détection des tumeurs à partir d’un code en python</p>
+                </div>
+                </br>
+            </div>
+        </div>    
+    </div>
+
+    <div class="container p-0">
+        <div class="row bg-dark text-white m-0 p-0" >
+            <div class=" col-12 text-center"></br><h2> <i class="fas fa-globe-europe" style="color: #ffffff;"></i> Mes engagements associastifs et expériences personnelles </h2></br></div>
+        </div>
+    </div> 
+
+    <div class="container p-0 ">
+        <div class="row align-self-center">
+            <div class="col-6 text-justify ">
+                </br>
+                <div class="row-2">
+                    <h4><img style="height: 50px" src="Artelier.png"> 2023 - 2024	: Comité Art'elier - Présidente</h4>
+                    <p>Organisation d'ateliers autours de l'art plastique, communication, gestion d'équipe</p>
+                </div>
+                </br>
+                <div class="row-2">
+                    <h4><img style="height: 50px" src="BDA.png"> 2023 - 2024	: Bureau des Arts - Co-Responsable Pôle communication</h4>
+                    <p>Rédaction des textes, préparation des plannings de publication et gestion d'une équipe</p>
+                </div>
+                </br>
+                <div class="row-2">
+                    <h4><img style="height: 50px" src="téléthon.png"> 2022 - 2023	: Comité Téléthon IMT Nord Europe - Pôle communication</h4>
+                    <p>Préparation des visuels, des plannings de publication et des goodies</p>
+                </div>
+            </div>
+            <div class="col-6 text-justify ">   
+                </br>
+                <div class="row-2">
+                    <h4><img style="height: 40px" src="Coree.png"> Août 2022 : Voyage en Corée (1 mois)</h4>
+                    <p>Organisation autonome</p>
+                    <p> Apprentissage du coréen à Ewha Womans University - Niveau 3 certifié </p>
+                </div>
+                </br>
+                <div class="row-2">
+                    <h4><img style="height: 50px" src="GR.png"> 2010 - 2020 : Gymnastique Rythmique (GR) - Seine-Essonne GR, St Germain lès Corbeil (91)</h4>
+                    <p>Compétitions sportives et galas</p>
+                    <p>Encadrement de jeunes lors des représentations et durant les cours</p>
+                </div>
+                </br>
+                <div class="row-2">
+                    <h4><i class="fas fa-ribbon" style="color: #e222b9;"></i> 2019, 2022 & 2023 :	Participation à la course Octobre Rose</h4>
+                    <p>Parc de Villeroy - Mennecy  (91) & Citadelle - Lille (59)</p>
+                    <p>Course solidaire au profit de la Ligue contre le cancer du sein</p>
+                </div>
+                </br>
+            </div>
+        </div>   
+
+	
+ 
+    </div> 
+
+    <?php require_once('template_footer.php')?>
+
+</body>
+
+</html>
