7457e8e97699fcc0f33a58144616204f055aec45
Anna Ruiz <anna.ruiz@free.fr>
2023-10-23 15:45:11 +0200

    exo 2 TP5 fonctionnel

--- TP5/exo2/users.js
+++ TP5/exo2/users.js
@@ -13,8 +13,8 @@ $(document).ready(function() {
                 data: null,
                 render: function (data, type, row) {
                     return `
-                        <button type="button" class="btn btn-primary" onclick="modifyRow(${row.id})">Modify</button>
-                        <button type="button" class="btn btn-danger" onclick="deleteRow(${row.id})">Delete</button>
+                        <button type="button" class="btn btn-primary" onclick="modifyRow(this)">Modify</button>
+                        <button type="button" class="btn btn-danger" onclick="deleteRow(this)">Delete</button>
                     `;
                 }
             }
@@ -35,8 +35,8 @@ $(document).ready(function() {
                     <td>${response.name}</td>
                     <td>${response.email}</td>
                     <td>
-                        <button type="button" class="btn btn-primary" onclick="modifyRow(${response.id})">Modify</button>
-                        <button type="button" class="btn btn-danger" onclick="deleteRow(${response.id})">Delete</button>
+                        <button type="button" class="btn btn-primary" onclick="modifyRow(this)">Modify</button>
+                        <button type="button" class="btn btn-danger" onclick="deleteRow(this)">Delete</button>
                     </td>
                 `;
 
@@ -52,30 +52,48 @@ $(document).ready(function() {
     });
 });
 
-function modifyRow(id) {
-    let row = $(`#users tbody tr[data-id="${id}"]`);
-    let name = row.find('td:eq(1)').text();
-    let email = row.find('td:eq(2)').text();
 
-    row.find('td:eq(1)').html(`<input type="text" value="${name}" />`);
-    row.find('td:eq(2)').html(`<input type="text" value="${email}" />`);
-    row.find('td:eq(3)').html(`<button type="button" class="btn btn-success" onclick="saveRow(${id})">Save</button>`);
+function modifyRow(button) {
+    // Sélectionner la ligne parente
+    let row = $(button).closest('tr');
+
+    // Récupérer les valeurs actuelles des cellules
+    let nom = row.find('td:eq(1)').text();
+    let mail = row.find('td:eq(2)').text();
+
+    // Remplacer le contenu des cellules par des champs de saisie pré-remplis
+    row.find('td:eq(1)').html(`<input type="text" value="${nom}" />`);
+    row.find('td:eq(2)').html(`<input type="text" value="${mail}" />`);
+
+    // Ajouter un bouton "Save"
+    row.find('td:eq(3)').html(`<button type="button" class="btn btn-success" onclick="saveRow(this)">Save</button>`);
 }
 
-function saveRow(id) {
-    let row = $(`#users tbody tr[data-id="${id}"]`);
+function saveRow(button) {
+    // Sélectionner la ligne parente
+    let row = $(button).closest('tr');
+
+    // Récupérer les nouvelles valeurs des champs de saisie``
+    let id = row.find('td:eq(0)').text();
     let name = row.find('input:eq(0)').val();
     let email = row.find('input:eq(1)').val();
+
+    row.find('td:eq(1)').text(name);
+    row.find('td:eq(2)').text(email);
+    row.find('td:eq(3)').html(`<button type="button" class="btn btn-primary" onclick="modifyRow(this)">Modify</button><button type="button" class="btn btn-danger" onclick="deleteRow(this)">Delete</button>`);
+        
+
 
     // Faire une requête pour mettre à jour les informations dans le backend
     $.ajax({
         type: 'PUT',
         url: 'http://localhost:8888/IDAW/TP4/exo5/users.php',
+        contentType: 'application/x-www-form-urlencoded',
         data: {id: id, name: name, email: email},
         success: function(response) {
             row.find('td:eq(1)').text(name);
             row.find('td:eq(2)').text(email);
-            row.find('td:eq(3)').html(`<button type="button" class="btn btn-primary" onclick="modifyRow(${id})">Modify</button><button type="button" class="btn btn-danger" onclick="deleteRow(${id})">Delete</button>`);
+            row.find('td:eq(3)').html(`<button type="button" class="btn btn-primary" onclick="modifyRow(this)">Modify</button><button type="button" class="btn btn-danger" onclick="deleteRow(this)">Delete</button>`);
         },
         error: function(error) {
             console.error(error);
@@ -83,19 +101,24 @@ function saveRow(id) {
     });
 }
 
-function deleteRow(id) {
-    let userId = id;
-    // Faire une requête pour supprimer l'utilisateur dans le backend
+function deleteRow(button) {
+    let row = $(button).closest('tr');
+
+    // Récupérer les valeurs actuelles des cellules
+    let id = row.find('td:eq(0)').text();
+    $(`#users tbody tr[data-id="${id}"]`).remove();
+
     $.ajax({
         type: 'DELETE',
         url: 'http://localhost:8888/IDAW/TP4/exo5/users.php',
         data: {id: id},
         success: function(response) {
-            $(`#users tbody tr[data-id="${userId}"]`).remove();
+            let table = $('#users').DataTable();
+            table.row(row).remove().draw();
 
         },
         error: function(error) {
             console.error(error);
         }
     });
-}
+    }
